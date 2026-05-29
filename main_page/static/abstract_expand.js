bb1dbfcb6c7298d618fd9d97d424561d17f4896c
Henri Dosku <hedosku@eurac.edu>
2026-05-29 12:30:25 +0200

    polish discovery toolbar UI and names of institutes

--- main_page/static/abstract_expand.js
+++ main_page/static/abstract_expand.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
 
-  $("#period_begin").datepicker();
-  $("#period_end").datepicker();
-
   $(".checkbox_categories_input").change(function () {
     if (typeof dosearch === "function") {
       dosearch();
